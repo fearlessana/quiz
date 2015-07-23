63bb36bb5f619047a498493c977a230dada5df9b
fearlessana <fearlessanaxo79@gmail.com>
2015-07-23 13:51:05 +0100

    Despliegue en Heroku

---
+++ controllers/creditController.js
@@ -0,0 +1,5 @@
+// GET /author
+exports.credit = function(req, res) {
+   res.render('author', {author: 'Ana C.C.'});
+};
+
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quizController');
+var creditController = require('../controllers/creditController');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-/* GET author page. 
-router.get('/author', function(req, res) {
-  res.render('author', { title: 'Ana CC' });
-});*/
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+router.get('/author', creditController.credit);
+
 
 module.exports = router;
