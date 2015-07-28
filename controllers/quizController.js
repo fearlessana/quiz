29cefb84cb0f3247827163da545a56f8a16d4253
fearlessana <fearlessanaxo79@gmail.com>
2015-07-28 12:16:36 +0100

    Modulo 7 - Buscador de Preguntas
    añadir el buscador en la página de preguntas

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,19 +1,43 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-   models.Quiz.findAll().then(function(quiz){
-   res.render('quizes/question', {pregunta: quiz[0].pregunta});	
-   })   
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+    	function(quiz) {
+      		if (quiz) {
+        		req.quiz = quiz;
+        		next();
+      		} else { next(new Error('No existe quizId=' + quizId)); }
+    	}
+  	).catch(function(error) { next(error);});
 };
 
-// GET /quizes/answer
+// GET /quizes
+exports.index = function(req, res) {
+	if(req.query.search) {
+    var filtro = (req.query.search || '').replace(" ", "%");
+    filtro = '%'+filtro+'%';
+    models.Quiz.findAll({where:["pregunta like ?", filtro],
+      order:'pregunta ASC'}).then(function(quizes) {
+        res.render('quizes/index', {quizes: quizes});
+        }).catch(function(error) { next(error);});
+  } else {
+    models.Quiz.findAll().then(function(quizes) {
+      res.render('quizes/index', {quizes: quizes});
+      }).catch(function(error) { next(error); });
+    }
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+	res.render('quizes/show', { quiz: req.quiz});	   
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-   models.Quiz.findAll().then(function(quiz){
-	   if (req.query.respuesta === quiz[0].respuesta){
-	      res.render('quizes/answer', {respuesta: 'Correcto'});
-	   } else {
-	      res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	   }
-	})
+    var resultado = "Incorrecto";
+    if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
+	    resultado = 'Correcto';
+	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,17 @@ var creditController = require('../controllers/creditController');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors:[]});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload
+// Definicion rutas de /quizes
+router.get('/quizes', quizController.index);
+//router.get('/quizes/index', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
 router.get('/author', creditController.credit);
 
 
