248c397b196fdacfdeb3320cb52a50c98251be6e
fearlessana <fearlessanaxo79@gmail.com>
2015-08-03 12:36:08 +0100

    Editar entradas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.name = 'Quiz';
+app.use(methodOverride('_method'));
 app.use(partials());
 
 // uncomment after placing your favicon in /public
