910e10a229f840f27aae55bd56c36e8696dcd636
fearlessana <fearlessanaxo79@gmail.com>
2015-08-05 12:40:59 +0100

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load); //autoload
+router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -30,6 +31,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', creditController.credit);
 module.exports = router;
